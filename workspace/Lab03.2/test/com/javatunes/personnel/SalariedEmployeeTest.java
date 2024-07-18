7b974b330ee38e28f3b232dbd5ec8e35b61b7759
Sherie Chandler <SherieC@tlgcohort.com>
2024-07-18 16:36:20 -0400

    lab03.2 testing

--- workspace/Lab03.2/test/com/javatunes/personnel/SalariedEmployeeTest.java
+++ workspace/Lab03.2/test/com/javatunes/personnel/SalariedEmployeeTest.java
@@ -9,10 +9,36 @@ import static org.junit.Assert.*;
 
 public class SalariedEmployeeTest {
     private SalariedEmployee emp;
+    private SalariedEmployee emp2;
 
     @Before
     public void setUp() {
         emp = new SalariedEmployee("John", Date.valueOf("2024-05-13"), 1500.00);
+        emp2 = new SalariedEmployee("John", Date.valueOf("2024-05-13"), 1500.00);
+    }
+
+    @Test
+    public void equals_shouldReturnFalse_differentName_sameHiredDate_sameSalary() {
+        emp2.setName("Lee");
+    }
+
+    @Test
+    public void equals_shouldReturnFalse_same_differentHireDate_sameSalary() {
+        emp2.setHireDate(Date.valueOf("2024-05-03"));
+    }
+
+    @Test
+    public void equals_shouldReturnFalse_sameName_sameHireDate_differentSalary() {
+        emp2.setSalary(1299.0);
+        assertNotEquals(emp, emp2);
+        assertFalse(emp.equals(emp2));
+    }
+
+    @Test
+    public void equals_shouldReturnTrue_allPropertiesSame() {
+        assertEquals(emp, emp);   // assertEquals() for objects does a .equals() check
+        assertTrue(emp.equals(emp));   // aternative
+
     }
 
     @Test
