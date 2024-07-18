29fcfa49123dab1cfb0ad0363048ffa7322bd479
Sherie Chandler <SherieC@tlgcohort.com>
2024-07-18 14:26:12 -0400

    added lifecycle methods

--- workspace/Lab03.1/test/com/math/CalculatorTest.java
+++ workspace/Lab03.1/test/com/math/CalculatorTest.java
@@ -9,24 +9,48 @@
 package com.math;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
+    // Business Objects(s) under test-called the "fixture".
+    private Calculator calc;
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before Class");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("setUp");
+        calc = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+    }
 
     @Test
     public void testIsEven() {
-        // Add implementation if needed
+        System.out.println("isEven");
+        assertTrue(calc.isEven(10));
+        assertTrue(calc.isEven(11));
     }
 
     @Test
     public void testDivide() {
-        Calculator calc = new Calculator();
-        assertEquals(2.5, calc.divide(5, 2), 0.001);
+        System.out.println("testDivide");
+        assertEquals(2.5, calc.divide(5, 2), 0.001); // expected, actual, delta
     }
 
     @Test
     public void testAdd() {
-        Calculator calc = new Calculator();
-        assertEquals(5, calc.add(1, 4));
+        System.out.println("testAdd");
+        assertEquals(5, calc.add(1, 4)); // expected, actual
     }
 }
