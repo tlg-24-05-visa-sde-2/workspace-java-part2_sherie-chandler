c97fd7d4e5c5e11413cc0509a41f6c5c9618655f
Sherie Chandler <SherieC@tlgcohort.com>
2024-07-18 12:27:29 -0400

    Lab03.1 complete-basic tests and assertions

--- workspace/Lab03.1/src/com/math/Calculator.java
+++ workspace/Lab03.1/src/com/math/Calculator.java
@@ -10,7 +10,8 @@ package com.math;
 
 public class Calculator {
 
-    public int add(int a, int b) {
+    public int add(int a, int b)
+    {
         return a + b;
     }
 
--- workspace/Lab03.1/test/com/math/CalculatorTest.java
+++ workspace/Lab03.1/test/com/math/CalculatorTest.java
@@ -14,8 +14,19 @@ import org.junit.Test;
 public class CalculatorTest {
 
     @Test
+    public void testIsEven() {
+        // Add implementation if needed
+    }
+
+    @Test
+    public void testDivide() {
+        Calculator calc = new Calculator();
+        assertEquals(2.5, calc.divide(5, 2), 0.001);
+    }
+
+    @Test
     public void testAdd() {
         Calculator calc = new Calculator();
-        assertEquals(5, calc.add(1, 4));  // expected, actual
+        assertEquals(5, calc.add(1, 4));
     }
 }
